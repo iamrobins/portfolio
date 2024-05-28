f5870693a7533a9e329487d6abe1f6808501cb64
Alex <42088724+iamrobins@users.noreply.github.com>
2024-05-28 21:44:00 +0000

    changed basic info

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -30,16 +30,16 @@ const AboutSection = () => {
               Get to know me!
             </h1>
             <p>
-              Hi, my name is Hosna and I am a{" "}
+              Hi, my name is Robin and I am a{" "}
               <span className="font-bold">{"highly ambitious"}</span>,
               <span className="font-bold">{" self-motivated"}</span>, and
               <span className="font-bold">{" driven"}</span> software engineer
-              based in Los Angeles, CA.
+              based in London, UK.
             </p>
             <br />
             <p>
-              I graduated from California State University, Northridge in 2019
-              with a BS in Computer Engineering and have been working in the
+              I graduated from University of Hertfordshire, Hatfield in 2024
+              with a MS in Business Analytics and have been working in the
               field ever since.
             </p>
             <br />
